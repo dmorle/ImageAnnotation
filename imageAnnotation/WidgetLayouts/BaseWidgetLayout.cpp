c8fa5534853eec330a619812af367e1aa3214673
dmorle <morled@uwindsor.ca>
2019-12-27 23:03:52 -0500

    temp commit of panel stuff

--- imageAnnotation/WidgetLayouts/BaseWidgetLayout.cpp
+++
@@ -1,9 +0,0 @@
-#include "BaseWidgetLayout.h"
-
-
-
-BaseWidgetLayout::BaseWidgetLayout(PRECT pRc, MainWindow* mw, Resizable* pRB)
-	: Widget(pRc, mw), Resizable(pRB)
-{
-	delete pRB;
-}
--- imageAnnotation/WidgetLayouts/WidgetLayouts.h
+++
@@ -1,6 +0,0 @@
-#ifndef WIDGETLAYOUTS_H
-#define WIDGETLAYOUTS_H
-
-// include file for all widget layouts
-
-#endif
